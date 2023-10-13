46d74e1f662cf58daa361d40349ed2d85cb0b176
Rolf Jordi <jordi.rolf@gmail.com>
2023-10-13 22:41:19 +0200

    fix unit test

--- tests/app.rs
+++ tests/app.rs
@@ -1,35 +1,30 @@
-use wasm_bindgen_test::{wasm_bindgen_test_configure, wasm_bindgen_test};
-use futures::prelude::*;
+use wasm_bindgen_test::*;
 use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::JsFuture;
 
-wasm_bindgen_test_configure!(run_in_browser);
+//wasm_bindgen_test_configure!(run_in_browser);
 
 
 // This runs a unit test in native Rust, so it can only use Rust APIs.
-#[test]
-fn rust_test() {
-    assert_eq!(1, 1);
-}
 
-
-// This runs a unit test in the browser, so it can use browser APIs.
 #[wasm_bindgen_test]
-fn web_test() {
+fn pass() {
     assert_eq!(1, 1);
 }
 
+#[wasm_bindgen_test]
+fn fail() {
+    assert_eq!(1, 1);
+}
 
-// This runs a unit test in the browser, and in addition it supports asynchronous Future APIs.
+//
+// // This runs a unit test in the browser, and in addition it supports asynchronous Future APIs.
 #[wasm_bindgen_test(async)]
-fn async_test() -> impl Future<Item = (), Error = JsValue> {
-    // Creates a JavaScript Promise which will asynchronously resolve with the value 42.
+async fn my_async_test() {
+    // Create a promise that is ready on the next tick of the micro task queue.
     let promise = js_sys::Promise::resolve(&JsValue::from(42));
 
-    // Converts that Promise into a Future.
-    // The unit test will wait for the Future to resolve.
-    JsFuture::from(promise)
-        .map(|x| {
-            assert_eq!(x, 42);
-        })
+    // Convert that promise into a future and make the test wait on it.
+    let x = JsFuture::from(promise).await.unwrap();
+    assert_eq!(x, 42);
 }
