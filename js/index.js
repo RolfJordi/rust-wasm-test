9453a8c653321b55adc131ab458c133ab3e83e58
Rolf Jordi <jordi.rolf@gmail.com>
2023-10-14 13:39:06 +0200

    use demo to set call back

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-import("../pkg/index.js").catch(console.error);
+import("../pkg/index.js")
+.then(wasm => {
+    document.getElementById("cal").textContent = wasm.addrust(1, 2);
+})
+.catch(console.error);
