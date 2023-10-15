a7719a37303f565a427ba6d0adbe50eece9fa9a8
Your Name <you@example.com>
2023-10-15 10:27:34 +0200

    fragtale

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 import("../pkg/index.js")
-.then(wasm => {
-    document.getElementById("cal").textContent = wasm.addrust(1, 2);
-})
-.catch(console.error);
+    .then(wasm => {
+        const canvas = document.getElementById('drawing');
+        const ctx = canvas.getContext('2d');
+
+        const realInput = document.getElementById('real');
+        const imaginaryInput = document.getElementById('imaginary');
+        const renderBtn = document.getElementById('render');
+
+        renderBtn.addEventListener('click', () => {
+            const real = parseFloat(realInput.value) || 0;
+            const imaginary = parseFloat(imaginaryInput.value) || 0;
+            wasm.draw(ctx, 600, 600, real, imaginary);
+        });
+
+        wasm.draw(ctx, 600, 600, -0.15, 0.65);
+    })
+    .catch(console.error);
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ use wasm_bindgen::prelude::*;
 use web_sys::console;
 use js_sys::{Array, Date};
 use web_sys::{Document, Element, HtmlElement, Window};
+use std::ops::Add;
+use wasm_bindgen::Clamped;
+use web_sys::{CanvasRenderingContext2d, ImageData};
+
 
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
@@ -13,129 +17,84 @@ use web_sys::{Document, Element, HtmlElement, Window};
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 
-// This is like the `main` function, except for JavaScript.
-#[wasm_bindgen(start)]
-fn run() -> Result<(), JsValue> {
-        // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
 
-    console::log_1(&JsValue::from_str("starting!"));
-
-    let window = web_sys::window().expect("should have a window in this context");
-    let document = window.document().expect("window should have a document");
-
-    // One of the first interesting things we can do with closures is simply
-    // access stack data in Rust!
-    let array = Array::new();
-    array.push(&"Hello".into());
-    array.push(&1.into());
-    let mut first_item = None;
-    array.for_each(&mut |obj, idx, _arr| match idx {
-        0 => {
-            assert_eq!(obj, "Hello");
-            first_item = obj.as_string();
-        }
-        1 => assert_eq!(obj, 1),
-        _ => panic!("unknown index: {}", idx),
-    });
-    assert_eq!(first_item, Some("Hello".to_string()));
-
-    // Below are some more advanced usages of the `Closure` type for closures
-    // that need to live beyond our function call.
-
-    setup_clock(&window, &document)?;
-    setup_clicker(&document);
-
-    // And now that our demo is ready to go let's switch things up so
-    // everything is displayed and our loading prompt is hidden.
-    document
-        .get_element_by_id("loading")
-        .expect("should have #loading on the page")
-        .dyn_ref::<HtmlElement>()
-        .expect("#loading should be an `HtmlElement`")
-        .style()
-        .set_property("display", "none")?;
-    document
-        .get_element_by_id("script")
-        .expect("should have #script on the page")
-        .dyn_ref::<HtmlElement>()
-        .expect("#script should be an `HtmlElement`")
-        .style()
-        .set_property("display", "block")?;
-
-    Ok(())
+#[wasm_bindgen]
+pub fn draw(
+    ctx: &CanvasRenderingContext2d,
+    width: u32,
+    height: u32,
+    real: f64,
+    imaginary: f64,
+) -> Result<(), JsValue> {
+    // The real workhorse of this algorithm, generating pixel data
+    let c = Complex { real, imaginary };
+    let data = get_julia_set(width, height, c);
+    let data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&data), width, height)?;
+    ctx.put_image_data(&data, 0.0, 0.0)
 }
 
-// Set up a clock on our page and update it each second to ensure it's got
-// an accurate date.
-//
-// Note the usage of `Closure` here because the closure is "long lived",
-// basically meaning it has to persist beyond the call to this one function.
-// Also of note here is the `.as_ref().unchecked_ref()` chain, which is how
-// you can extract `&Function`, what `web-sys` expects, from a `Closure`
-// which only hands you `&JsValue` via `AsRef`.
-fn setup_clock(window: &Window, document: &Document) -> Result<(), JsValue> {
-    let current_time = document
-        .get_element_by_id("current-time")
-        .expect("should have #current-time on the page");
-    update_time(&current_time);
-    let a = Closure::<dyn Fn()>::new(move || update_time(&current_time));
-    window
-        .set_interval_with_callback_and_timeout_and_arguments_0(a.as_ref().unchecked_ref(), 1000)?;
-    fn update_time(current_time: &Element) {
-        current_time.set_inner_html(&String::from(
-            Date::new_0().to_locale_string("en-GB", &JsValue::undefined()),
-        ));
+fn get_julia_set(width: u32, height: u32, c: Complex) -> Vec<u8> {
+    let mut data = Vec::new();
+
+    let param_i = 1.5;
+    let param_r = 1.5;
+    let scale = 0.005;
+
+    for x in 0..width {
+        for y in 0..height {
+            let z = Complex {
+                real: y as f64 * scale - param_r,
+                imaginary: x as f64 * scale - param_i,
+            };
+            let iter_index = get_iter_index(z, c);
+            data.push((iter_index / 4) as u8);
+            data.push((iter_index / 2) as u8);
+            data.push(iter_index as u8);
+            data.push(255);
+        }
     }
 
-    // The instance of `Closure` that we created will invalidate its
-    // corresponding JS callback whenever it is dropped, so if we were to
-    // normally return from `setup_clock` then our registered closure will
-    // raise an exception when invoked.
-    //
-    // Normally we'd store the handle to later get dropped at an appropriate
-    // time but for now we want it to be a global handler so we use the
-    // `forget` method to drop it without invalidating the closure. Note that
-    // this is leaking memory in Rust, so this should be done judiciously!
-    a.forget();
-
-    Ok(())
+    data
 }
 
-// We also want to count the number of times that our green square has been
-// clicked. Our callback will update the `#num-clicks` div.
-//
-// This is pretty similar above, but showing how closures can also implement
-// `FnMut()`.
-fn setup_clicker(document: &Document) {
-    let num_clicks = document
-        .get_element_by_id("num-clicks")
-        .expect("should have #num-clicks on the page");
-    let mut clicks = 0;
-    let a = Closure::<dyn FnMut()>::new(move || {
-        clicks += 1;
-        num_clicks.set_inner_html(&clicks.to_string());
-    });
-    document
-        .get_element_by_id("green-square")
-        .expect("should have #green-square on the page")
-        .dyn_ref::<HtmlElement>()
-        .expect("#green-square be an `HtmlElement`")
-        .set_onclick(Some(a.as_ref().unchecked_ref()));
-
-    // See comments in `setup_clock` above for why we use `a.forget()`.
-    a.forget();
+fn get_iter_index(z: Complex, c: Complex) -> u32 {
+    let mut iter_index: u32 = 0;
+    let mut z = z;
+    while iter_index < 900 {
+        if z.norm() > 2.0 {
+            break;
+        }
+        z = z.square() + c;
+        iter_index += 1;
+    }
+    iter_index
 }
 
-//creation d'une methode en js
-#[wasm_bindgen]
-pub fn addrust(a: u32, b: u32) -> u32 {
-    a + b
+#[derive(Clone, Copy, Debug)]
+struct Complex {
+    real: f64,
+    imaginary: f64,
 }
 
-#[wasm_bindgen]
-pub fn greet(a: &str) -> String {
-    format!("Hello, {}!", a)
+impl Complex {
+    fn square(self) -> Complex {
+        let real = (self.real * self.real) - (self.imaginary * self.imaginary);
+        let imaginary = 2.0 * self.real * self.imaginary;
+        Complex { real, imaginary }
+    }
+
+    fn norm(&self) -> f64 {
+        (self.real * self.real) + (self.imaginary * self.imaginary)
+    }
+}
+
+impl Add<Complex> for Complex {
+    type Output = Complex;
+
+    fn add(self, rhs: Complex) -> Complex {
+        Complex {
+            real: self.real + rhs.real,
+            imaginary: self.imaginary + rhs.imaginary,
+        }
+    }
 }
