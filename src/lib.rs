3e84808ec122f6387ba537d6c622a06da2399614
Rolf Jordi <jordi.rolf@gmail.com>
2023-10-15 11:58:44 +0200

    remove wee_alloc
    not maintained any more

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,6 @@ use std::ops::Add;
 use wasm_bindgen::Clamped;
 use web_sys::{CanvasRenderingContext2d, ImageData};
 
-
-
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-
-
 #[wasm_bindgen]
 pub fn draw(
     ctx: &CanvasRenderingContext2d,
